e9a97c7a115949b706b282fd5e0a57b0a9c07e87
milo <milo.namir@gmail.com>
2021-01-10 13:01:50 +0200

    Add project files.

---
+++ milo_example55/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace milo_example55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Temp\my_file.txt";
+
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine("Milo test: " + DateTime.Now.ToString());
+            }
+        }
+    }
+}
